ca6448cd57d46e5076ce1da85a54d0d92ead8f25
purushkumar1997 <purushottamk.1997@gmail.com>
2024-10-10 13:07:05 +0530

    added case for handling new line in input

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -15,3 +15,7 @@ test("It will return sum of two numbers given as comma seperated string", () => 
 test("It will return sum of more numbers given as comma seperated string", () => {
     expect(addString("4,5,10,34")).toBe(53);
 })
+
+test("It will give sum of numbers if new line will get introduced in string as input", () => {
+    expect(addString("4\n5,3")).toBe(12);
+})
