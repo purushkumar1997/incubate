5d59302a214f9d09c45af91c347db668421d4ee5
purushkumar1997 <purushottamk.1997@gmail.com>
2024-10-10 12:47:28 +0530

    added test for single number which failed

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -3,3 +3,7 @@ const addString = require("../src/calculator");
 test("It will return 0 if an empty string", () => {
     expect(addString("")).toBe(0);
 })
+
+test("It will return single number if there is a single number", () => {
+    expect(addString("7")).toBe(7);
+})
