bfb83f94c2a72a729c31fdc31474f386ce3ee067
purushkumar1997 <purushottamk.1997@gmail.com>
2024-10-10 13:35:11 +0530

    added logic to handle custom delimeters

--- src/calculator.js
+++ src/calculator.js
@@ -3,7 +3,14 @@ function addString(numStr) {
         return 0;
     }
     
-    const splitStr = new RegExp('[,\n]');
+    let splitStr = new RegExp('[,\n]');
+
+    if (numStr.startsWith("//")) {
+        const newLine = numStr.indexOf("\n");
+        const customDelim = numStr.substring(2, newLine);
+        splitStr = new RegExp(customDelim);
+        numStr = numStr.substring(newLine + 1);
+    }
     const numArray = numStr.split(splitStr);
     let sum = 0;
     numArray.forEach((val) => {
